8fffdda602df6dc5bc621828d574bb76f83faabd
Sam Kadmon <omaramzi00@gmail.com>
2024-04-27 13:31:08 +0200

    Initial Commit, basic window manager functionality (Many issues to iron out)

---
+++ Planning.md
@@ -0,0 +1,47 @@
+# MacroSun Dao
+
+# Rough Categories/Tags/Ideas, Moodboard (But in text form i guess???)
+- Cultivation Game
+- Infinite Reincarnation, Purchase upgrades in afterlife based on progress made in game.
+- Maybe an infinite timeloop?
+- - Maybe find special resources in life to progress?
+- - Immortality as a goal? Dominion over existence itself maybe?
+- STARTS AS REINCARNATION WITHOUT ANY MEMORY AT ALL, EVENTUALLY TURNS INTO REINCARNATION WITH FULL MEMORY.
+- - The degree to which you remember is based on best milestone achieved in cultivation / reincarnation upgrades
+- Every life should have its own circumstances, leading the player to try different paths, before gaining the ability to 'solidify' (Be always able to pick the same path in another life) their path as they remember more and more of their past incarnations
+
+# Concrete Progression
+- Mortal with no talent nor path in life, even beggars can do better than him. Maybe because of his infinite reincarnation talent awakening or something causing him to lose all skills, or maybe this is life's way of making it up to him and allowing this incompetent lout a *chance* at something greater.
+
+- Initial progression from when you start the game is going to be composed of really short lives where the player desperately tries to raise their skill levels to the point where they can survive in future loops/incarnations. 
+
+- - Maybe they are afflicted by extreme maladies of the mind body and soul initially, making things very difficult and death overwhelmingly likely
+
+# Primary Stats
+Stats that directly affect the player's capabilities
+
+## Physical Stats
+
+## Physical/Mental Stats
+
+## Mental Stats
+
+## Mental/Spiritual Stats
+
+## Spiritual Stats
+
+## Spiritual/Physical Stats
+
+- Lifespan: Self-evident. Basically a clock until the run ends, can be used as currency in runs for certain arts
+- Strength: Physical strength, crucial for physical combat, blacksmithing, certain events etc.
+- Endurance: 
+- Agility:
+- Dexterity:
+- Intelligence:
+- Wisdom:
+- Destiny:
+- Luck:
+# Secondary Stats
+Stats that are a confluence of the primary stats, still essential to the player's continued existence
+
+implement THA GAYM
---
+++ scripts/windowlogic.js
@@ -0,0 +1,133 @@
+//const scripttext ="$(document).ready(function(){$( '.resizable' ).resizable({alsoResize: '.window-body', handles: 'w, sw, s, se, e'});$( '.draggable' ).draggable();});";  
+
+function createWindow(title, contents) 
+{
+    var body = document.querySelector("body");
+	var window = document.createElement("div");
+	window.className = "window draggable resizable";
+    window.setAttribute('isMaximized',0);
+
+	var titlebar = document.createElement("div");
+	titlebar.className = "title-bar";
+
+    var titletext = document.createElement("div");
+    titletext.className = "title-bar-text";
+	titletext.innerText = title;
+    
+    var windowcontrols = document.createElement("div");
+    windowcontrols.className = "title-bar-controls";
+
+    var minimize = document.createElement("button");
+    minimize.setAttribute('aria-label',"Minimize");
+    var maximize = document.createElement("button");
+    maximize.setAttribute('aria-label',"Maximize");
+    var close = document.createElement("button");
+    close.setAttribute('aria-label',"Close");
+
+    var windowcontentarea = document.createElement("div");
+    windowcontentarea.className = "window-body";
+    
+    var windowcontents = document.createElement("object");
+    windowcontents.setAttribute("type","text/html");
+    windowcontents.setAttribute("data",contents);
+
+    
+
+    // Create the taskbar button representing the window
+    // var taskbarbutton = document.createElement("button");
+    // taskbarbutton.className = "nav-item";
+    // taskbarbutton.innerText = title;
+    // taskbarbutton.setAttribute('onclick', 'toggleElementDisplay(document.getElementById("'+ display.id + '"),"block");');
+    
+    // Attach all the created pieces of the window to eachother and the body of the site
+    windowcontrols.appendChild(minimize);
+    windowcontrols.appendChild(maximize);
+    windowcontrols.appendChild(close);
+    titlebar.appendChild(titletext);
+    titlebar.appendChild(windowcontrols);
+    windowcontentarea.appendChild(windowcontents);
+	window.appendChild(titlebar);
+    window.appendChild(windowcontentarea);
+	body.appendChild(window);
+        
+    $(".draggable").each(function ()
+    {
+        $(this)
+        .draggable( 
+            { 
+                addClasses: false, 
+                iframeFix: 
+                true, 
+                stack: ".window" 
+            })
+        .resizable(
+            { 
+                addClasses: false, 
+                handles: "all", 
+                alsoResize: $(this).find($(windowcontentarea)), 
+                addClasses: false 
+            });
+    } ); 
+     
+    // $('div').each(function () {
+    //     $(this).resizable({
+    //         alsoResize: $(this).find('img'),
+    //         aspectRatio: true,
+    //         maxHeight: 140
+    //     });
+    
+    // });  
+}
+
+function setTop(window)
+{
+    windowTable = document.getElementsByClassName("draggable");
+    var maxZ = windowTable[0];
+    for (let i = 0; i < windowTable.length; i++) 
+    {
+        if (windowTable[i] > maxZ) {maxZ = windowTable[i]};
+    }
+    for (let i = 0; i < windowTable.length; i++) {
+        if (window == windowTable[i]) 
+        {
+            windowTable.zIndex = maxZ + 1;
+        }
+    }
+}
+
+function refreshTime() {
+    var date = new Date();  
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours < 12 ? ampm = " AM" : " PM"; 
+    
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+ minutes : minutes;
+    var strTime = hours + ':' + minutes + ampm;
+    document.getElementsByClassName("system-clock")[0].innerText = strTime;
+}
+
+setInterval(refreshTime, 1000);
+// Mouse Event Listeners
+
+document.addEventListener('click', e => {
+    // Close the window
+	if (e.target.closest('[aria-label="Close"]')) 
+    {
+		e.target.closest('.window').remove();
+	}
+    if (e.target.closest('.taskbar-startbutton')) 
+    {
+		createWindow("Window Spawn Testing", "spawntest.html");
+	}
+});
+
+
+document.addEventListener('mousedown', e => {
+    // Select Window and put it to the top of all the other windows
+    if (e.target.closest('.window'))
+    {
+        setTop(e.target.closest('.window'));
+    }
+});
